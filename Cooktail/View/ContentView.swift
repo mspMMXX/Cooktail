6a5959c4367dc7b9e5c10d82d2e873235ac9dd53
MSP Markus <m.platter@die-fabrik.net>
2023-11-09 11:45:28 +0100

    Start here! :)

--- Cooktail/View/ContentView.swift
+++ Cooktail/View/ContentView.swift
@@ -11,20 +11,7 @@ struct ContentView: View {
     
     var body: some View {
         NavigationStack{
-            List(1..<101){ value in
-                NavigationLink("Zelle \(value)"){
-                    SecondContentView(number: value)
-                }
-            }
-            .navigationTitle("Cooktail")
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar(content: {
-                Button(action: {
-                    print("Plus tapped")
-                }, label: {
-                    Image(systemName: "plus")
-                })
-            })
+            Text("Hello Cooktail")
         }
     }
 }
